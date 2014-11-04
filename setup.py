944cfa6c29784eb6e32d5135361b85fe20a03390
Wendell Smith <wackywendell@gmail.com>
2014-11-04 14:32:11 -0500

    Sort of working

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 import os.path
 import glob
 
@@ -18,19 +18,41 @@ include_dirs = [ '/usr/local/include', 'src' ]
 library_dirs = [ '/usr/local/lib' ]
  
 # define the libraries to link with the boost python library
-libraries = [ 'boost_python' ]
+libraries = [ 'boost_python3' ]
  
 # define the source files for the extension
-source_files = ['cell.cc', 'common.cc', 'container.cc', 'unitcell.cc', 
+source_files = ['pyvoro.cc', 'cell.cc', 'common.cc', 'container.cc', 'unitcell.cc', 
                 'v_compute.cc', 'c_loops.cc', 'v_base.cc', 'wall.cc',
                 'pre_container.cc', 'container_prd.cc']
 source_files = [os.path.join('src', fname) for fname in source_files]
 
-print(source_files)
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 # create the extension and add it to the python distribution
 setup( name=extension_name, 
         version=extension_version, 
+        author="Wendell Smith",
+        author_email="wackywendell@gmail.com",
+        description = ("A odule for calculating and analyzing Voronoi tesselations"),
+        license = "BSD",
+        keywords = "laguerre voronoi tesselation",
+        url = "https://github.com/wackywendell/pyvoro",
+        long_description=read('README.md'),
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Topic :: Scientific/Engineering :: Mathematics",
+            "Topic :: Scientific/Engineering :: Physics",
+            "Intended Audience :: Science/Research",
+            "Operating System :: POSIX :: Linux",
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: BSD License",
+        ],
+
         ext_modules=[Extension(
             extension_name, source_files, 
             include_dirs=include_dirs,
