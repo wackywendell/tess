e0f598a5c73d853863db0a39f2137b87071beea8
Wendell Smith <wackywendell@gmail.com>
2014-11-13 11:56:11 -0500

    Compiles!

--- setup.py
+++ setup.py
@@ -1,33 +1,9 @@
-from setuptools import setup, Extension
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
 import os.path
-import sys
-import glob
 
 extension_version = '0.1'
- 
-# define the directories to search for include files
-# to get this to work, you may need to include the path
-# to your boost installation. Mine was in
-# '/usr/local/include', hence the corresponding entry.
-include_dirs = [ '/usr/local/include', 'src' ]
- 
-# define the library directories to include any extra
-# libraries that may be needed.  The boost::python
-# library for me was located in '/usr/local/lib'
-library_dirs = [ '/usr/local/lib' ]
- 
-# define the libraries to link with the boost python library
-
-libraries = [ 'boost_python3' if sys.version_info.major == 3 else 'boost_python' ]
- 
-# define the source files for the extension
-source_files = ['src/voro++.cc', 'tess/voro.cc']
-
-extension = Extension(
-            'tess._voro', source_files, 
-            include_dirs=include_dirs,
-            library_dirs=library_dirs,
-            libraries=libraries)
 
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
@@ -35,6 +11,13 @@ extension = Extension(
 # string in below ...
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    
+ext = Extension("tess._voro",
+              sources=["tess/_voro.pyx", "src/voro++.cc"],
+              include_dirs=["src"],
+              language="c++",
+              )
+extensions = cythonize(ext)
 
 # create the extension and add it to the python distribution
 setup( name='tess', 
@@ -59,4 +42,4 @@ setup( name='tess',
         packages=['tess'],
         package_dir={"tess": "tess"},
         
-        ext_modules=[extension])
+        ext_modules = extensions)
