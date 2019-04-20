c347fa188e11a974c55ddfe93032b9b7caaf5713
Wendell Smith <wackywendell@gmail.com>
2019-04-20 11:15:46 -0400

    Applied black

--- tess/tests.py
+++ tess/tests.py
@@ -9,9 +9,11 @@ try:
 except ImportError:
     scipy = None
 
+
 class LatticeTest:
     """A basic test for testing a lattice. The basic constants below need to be overwritten,
     as well as the setUp() function."""
+
     limits = None
     order = None
 
@@ -33,24 +35,39 @@ class LatticeTest:
             self.assertEqual(n, fn)
 
     def test_order(self):
-        if scipy is None: return
-        if self.order is None: return
-        self.assertAlmostEqual(self.order, self.cells.order(l=6, weighted=True, local=True))
-        self.assertAlmostEqual(self.order, self.cells.order(l=6, weighted=True, local=False))
-        self.assertAlmostEqual(self.order, self.cells.order(l=6, weighted=False, local=True))
-        self.assertAlmostEqual(self.order, self.cells.order(l=6, weighted=False, local=False))
+        if scipy is None:
+            return
+        if self.order is None:
+            return
+        self.assertAlmostEqual(
+            self.order, self.cells.order(l=6, weighted=True, local=True)
+        )
+        self.assertAlmostEqual(
+            self.order, self.cells.order(l=6, weighted=True, local=False)
+        )
+        self.assertAlmostEqual(
+            self.order, self.cells.order(l=6, weighted=False, local=True)
+        )
+        self.assertAlmostEqual(
+            self.order, self.cells.order(l=6, weighted=False, local=False)
+        )
+
 
 class CubicLattice(LatticeTest):
     n = 4
-    limits = (n,n,n)
+    limits = (n, n, n)
 
-    def get_points(self, r2 = 0.6):
+    def get_points(self, r2=0.6):
         nm = self.n
-        ptsr = [tuple(np.array((l,m,n))+.5) + ((l+m+n) % 2,)
-                for l in range(nm) for m in range(nm) for n in range(nm)]
+        ptsr = [
+            tuple(np.array((l, m, n)) + 0.5) + ((l + m + n) % 2,)
+            for l in range(nm)
+            for m in range(nm)
+            for n in range(nm)
+        ]
         ptsr = np.array(ptsr)
         pts = ptsr[:, :3]
-        r = [.5 if r < 1 else r2 for r in ptsr[:, 3]]
+        r = [0.5 if r < 1 else r2 for r in ptsr[:, 3]]
         return pts, r
 
     def setUp(self):
@@ -58,134 +75,166 @@ class CubicLattice(LatticeTest):
         self.cells = Container(self.pts, self.n, periodic=False)
 
     def test_str(self):
-        self.assertEqual(str(self.cells[0]), '<Cell 0>')
-        self.assertEqual(repr(self.cells[0]), '<Cell 0>')
-        self.assertEqual(str(self.cells[1]), '<Cell 1>')
-        self.assertEqual(repr(self.cells[1]), '<Cell 1>')
-        self.assertEqual(str(self.cells[-1]), '<Cell {0}>'.format(len(self.cells)-1))
-        self.assertEqual(repr(self.cells[-1]), '<Cell {0}>'.format(len(self.cells)-1))
+        self.assertEqual(str(self.cells[0]), "<Cell 0>")
+        self.assertEqual(repr(self.cells[0]), "<Cell 0>")
+        self.assertEqual(str(self.cells[1]), "<Cell 1>")
+        self.assertEqual(repr(self.cells[1]), "<Cell 1>")
+        self.assertEqual(str(self.cells[-1]), "<Cell {0}>".format(len(self.cells) - 1))
+        self.assertEqual(repr(self.cells[-1]), "<Cell {0}>".format(len(self.cells) - 1))
 
 
 class BasicCubic(CubicLattice, TestCase):
     order = 0.35355339059328
-    
-    def test_volumes(self): self.volumes([1.0])
-        
-    def test_neighbors(self): self.neighbors([6])
-    
+
+    def test_volumes(self):
+        self.volumes([1.0])
+
+    def test_neighbors(self):
+        self.neighbors([6])
+
     def test_centroid(self):
         cx, cy, cz = self.cells[0].centroid()
         self.assertAlmostEqual(cx, 0.5)
         self.assertAlmostEqual(cy, 0.5)
         self.assertAlmostEqual(cz, 0.5)
-    
+
     def test_face_areas(self):
         areas = self.cells[0].face_areas()
         self.assertEqual(len(areas), 6)
         for a in areas:
             self.assertAlmostEqual(a, 1.0)
-    
+
     def test_radii(self):
-        for c,r in zip(self.cells, self.r):
+        for c, r in zip(self.cells, self.r):
             self.assertAlmostEqual(c.radius, 0)
+
 
 class CubicAlternating(CubicLattice, TestCase):
     def setUp(self):
         self.pts, self.r = self.get_points(r2=0.6)
         self.cells = Container(self.pts, self.n, radii=self.r, periodic=True)
-        
-    def test_volumes(self): self.volumes([1.295031, 0.704969])
-    
+
+    def test_volumes(self):
+        self.volumes([1.295031, 0.704969])
+
     def test_radii(self):
-        for c,r in zip(self.cells, self.r):
+        for c, r in zip(self.cells, self.r):
             self.assertAlmostEqual(c.radius, r)
+
 
 class CubicBlocks(CubicLattice, TestCase):
     def setUp(self):
         self.pts, self.r = self.get_points(r2=0.5)
         self.cells = Container(self.pts, self.n, periodic=False, blocks=2)
-    
+
     def test_blocks(self):
-        self.assertEqual(self.cells.blocks, (2,2,2))
+        self.assertEqual(self.cells.blocks, (2, 2, 2))
+
 
 class CubicBlocksUnequal(CubicLattice, TestCase):
     def setUp(self):
         self.pts, self.r = self.get_points(r2=0.5)
-        self.cells = Container(self.pts, self.n, periodic=False, blocks=(0,1.1,2))
+        self.cells = Container(self.pts, self.n, periodic=False, blocks=(0, 1.1, 2))
 
     def test_blocks(self):
-        self.assertEqual(self.cells.blocks, (1,1,2))
+        self.assertEqual(self.cells.blocks, (1, 1, 2))
+
 
 class FCC(LatticeTest, TestCase):
     order = 0.57452425971404164
     n = 4
-    limits = np.array((n,n,n))
-    
+    limits = np.array((n, n, n))
+
     def points(self):
-        x0 = np.array((0,0,0))
-        x1 = np.array((0,0.5,0.5))
-        x2 = np.array((0.5,0,0.5))
-        x3 = np.array((0.5,0.5,0))
-        
+        x0 = np.array((0, 0, 0))
+        x1 = np.array((0, 0.5, 0.5))
+        x2 = np.array((0.5, 0, 0.5))
+        x3 = np.array((0.5, 0.5, 0))
+
         n1, n2, n3 = self.limits
-        dxs = np.array([(i,j,k) for k in range(n3) for j in range(n2) for i in range(n1)])
-        xs = np.array([[x+dx for x in (x0, x1, x2, x3)] for dx in dxs])
+        dxs = np.array(
+            [(i, j, k) for k in range(n3) for j in range(n2) for i in range(n1)]
+        )
+        xs = np.array([[x + dx for x in (x0, x1, x2, x3)] for dx in dxs])
         return np.concatenate(xs)
-    
+
     def setUp(self):
         # also use non-origin limits
         self.cells = Container(self.points(), limits=self.limits, periodic=True)
-    
-    def test_volumes(self): self.volumes([0.25])
-    
-    def test_neighbors(self): self.neighbors([12])
+
+    def test_volumes(self):
+        self.volumes([0.25])
+
+    def test_neighbors(self):
+        self.neighbors([12])
+
 
 class FCCelongated(FCC):
     n = 4
-    limits = np.array((n,n,2*n))
-    
+    limits = np.array((n, n, 2 * n))
+
     def test_blocks(self):
         self.assertEqual(tuple(self.cells.blocks), (6, 6, 13))
+
 
 class FCCnegative(FCC):
     def setUp(self):
         # also use non-origin limits
-        self.cells = Container(self.points(), limits=(-self.limits/2., self.limits/2.), periodic=True)
+        self.cells = Container(
+            self.points(), limits=(-self.limits / 2.0, self.limits / 2.0), periodic=True
+        )
+
 
 class FCCmoved(FCC):
     def setUp(self):
         # also use non-origin limits
-        self.cells = Container(self.points(), limits=(self.limits/2., self.limits*3./2.), periodic=True)
+        self.cells = Container(
+            self.points(),
+            limits=(self.limits / 2.0, self.limits * 3.0 / 2.0),
+            periodic=True,
+        )
+
 
 class FCCsinglelimit(FCC):
     def setUp(self):
         # also use non-origin limits
         self.cells = Container(self.points(), limits=self.n, periodic=True)
 
+
 class HCP(FCC):
     order = 0.48476168522368324
     n = 4
-    limits = np.array((n, n*sqrt(3.0)/2, n*sqrt(6.)/3.))
-    
+    limits = np.array((n, n * sqrt(3.0) / 2, n * sqrt(6.0) / 3.0))
+
     def points(self):
         assert self.n % 2 == 0
-        arr = np.array([(
-                            2*i + (j%2) + (k%2),
-                            sqrt(3.0)*(j + (k%2)/3.),
-                            sqrt(24.)*k/3)
-                         for k in range(self.n) for j in range(self.n) for i in range(self.n)])
-        return arr/2.
-    
+        arr = np.array(
+            [
+                (
+                    2 * i + (j % 2) + (k % 2),
+                    sqrt(3.0) * (j + (k % 2) / 3.0),
+                    sqrt(24.0) * k / 3,
+                )
+                for k in range(self.n)
+                for j in range(self.n)
+                for i in range(self.n)
+            ]
+        )
+        return arr / 2.0
+
     def setUp(self):
         self.cells = Container(self.points(), limits=self.limits, periodic=True)
-    
-    def test_volumes(self): self.volumes([1./sqrt(2.)])
-    
-    def test_neighbors(self): self.neighbors([12])
+
+    def test_volumes(self):
+        self.volumes([1.0 / sqrt(2.0)])
+
+    def test_neighbors(self):
+        self.neighbors([12])
+
 
 class TestBoundaries(TestCase):
     limits = ((-50, -20, 80), (-30, -10, 120))
-    
+
     def test_container(self):
         xs = [-31, -49]
         ys = [-11, -19]
