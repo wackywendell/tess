e6465a6f36fe3d498634e31727ce3783111b131b
Wendell Smith <wackywendell@gmail.com>
2019-04-20 12:48:09 -0400

    Format setup.py

--- setup.py
+++ setup.py
@@ -33,22 +33,30 @@ try:
 except ImportError:
     cythonize = None
 
-extension_version = '0.2.2'
+extension_version = "0.2.2"
 
 if cythonize is not None:
     print("Building with Cython.")
-    ext = cythonize([Extension("tess._voro",
-              sources=["tess/_voro.pyx", "src/voro++.cc"],
-              include_dirs=["src"],
-              language="c++",
-              )])
+    ext = cythonize(
+        [
+            Extension(
+                "tess._voro",
+                sources=["tess/_voro.pyx", "src/voro++.cc"],
+                include_dirs=["src"],
+                language="c++",
+            )
+        ]
+    )
 else:
     print("Cython not found, using prebuilt file.")
-    ext = [Extension("tess._voro",
-              sources=["tess/_voro.cpp", "src/voro++.cc"],
-              include_dirs=["src"],
-              language="c++",
-              )]
+    ext = [
+        Extension(
+            "tess._voro",
+            sources=["tess/_voro.cpp", "src/voro++.cc"],
+            include_dirs=["src"],
+            language="c++",
+        )
+    ]
 
 # Set sdist to make the .cpp file
 # from http://stackoverflow.com/a/18418524/4190270
@@ -56,36 +64,36 @@ class sdist(_sdist):
     def run(self):
         # this is already imported, but the import might have failed. If so, raise an ImportError now.
         from Cython.Build import cythonize
-        
+
         # Make sure the compiled Cython files in the distribution are up-to-date
-        cythonize(['tess/_voro.pyx'])
+        cythonize(["tess/_voro.pyx"])
         _sdist.run(self)
 
-cmdclass = dict(sdist = sdist)
+
+cmdclass = dict(sdist=sdist)
 
 # create the extension and add it to the python distribution
-setup( name='tess', 
-        version=extension_version, 
-        author="Wendell Smith",
-        author_email="wackywendell@gmail.com",
-        description = ("A module for calculating and analyzing Voronoi tessellations"),
-        license = "BSD",
-        keywords = "laguerre voronoi tessellation voro++",
-        url = "https://tess.readthedocs.org",
-        long_description=__doc__.strip(),
-        classifiers=[
-            "Development Status :: 3 - Alpha",
-            "Topic :: Scientific/Engineering :: Mathematics",
-            "Topic :: Scientific/Engineering :: Physics",
-            "Intended Audience :: Science/Research",
-            "Operating System :: POSIX :: Linux",
-            "Programming Language :: Python :: 3",
-            "License :: OSI Approved :: BSD License",
-        ],
-        
-        packages=['tess'],
-        package_dir={"tess": "tess"},
-        
-        ext_modules = ext,
-        cmdclass=cmdclass,
-        )
+setup(
+    name="tess",
+    version=extension_version,
+    author="Wendell Smith",
+    author_email="wackywendell@gmail.com",
+    description=("A module for calculating and analyzing Voronoi tessellations"),
+    license="BSD",
+    keywords="laguerre voronoi tessellation voro++",
+    url="https://tess.readthedocs.org",
+    long_description=__doc__.strip(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Intended Audience :: Science/Research",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+    ],
+    packages=["tess"],
+    package_dir={"tess": "tess"},
+    ext_modules=ext,
+    cmdclass=cmdclass,
+)
